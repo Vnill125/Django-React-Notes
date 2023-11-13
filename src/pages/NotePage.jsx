2ecee8e7330c3df85ff6297bd43b65f1da204f10
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-13 21:43:47 +0100

    Update NotePage.jsx

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -1,1 +1,34 @@
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import {FaArrowLeft} from 'react-icons/fa'
 
+
+const NotePage = () => {
+   
+    let {noteId} = useParams()
+    let [note, setNote] = useState(null)
+
+    useEffect(() => {
+        getNote()
+    }, [])
+
+    let getNote = async () => {
+        let response = await fetch(`http://127.0.0.1:8000/api/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+  return (
+    <div className='note'>
+      <div className='note-header'>
+        <h3>
+          <Link to={'/'}><FaArrowLeft/></Link>
+        </h3>
+      </div>
+      <textarea onChange={(e) => {setNote({...note, 'body':e.target.value}) }} defaultValue={note?.body}></textarea>
+    </div>
+  )
+}
+
+export default NotePage
