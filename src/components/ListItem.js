ea44b65bab76a983bf7e661e17778c0beda45884
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-13 21:42:58 +0100

    Update ListItem.js

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const ListItem = ({note}) => {
+
+const ListItem = ({index, note}) => {
   return (
-    <div>
-      <h3>{note.body}</h3>
-    </div>
+    <Link to={`/note/${index + 1}`}>
+      <div className='notes-list-item'>
+        <h3>{note.body}</h3>
+      </div>
+      
+    </Link>
   )
 }
 
