4128dbc25a9252a62da3c504cb2f0eea9c07f085
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-13 21:44:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { BrowserRouter as Router, Route}from 'react-router-dom'
+import Header from './components/Header'
+import NotesListPage from './pages/NotesListPage'
+import NotePage from './pages/NotePage'
+import './App.css'
 
-import Header from './components/Header.js'
-import NotesListPage from './pages/NotesListPage.js';
-import './App.css';
+import  {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
+
   return (
     <Router>
-    <div className="App">
-      <Header/>
-      <Route path='/' exact component={NotesListPage}>
-    </div>
+      <div className='container dark'>
+        <div className='app'>
+        <Header/>
+
+          <Routes>
+            <Route path='/' exact Component={NotesListPage}/>
+            <Route path='/note/:noteId' element={<NotePage/>}/>
+          </Routes>
+        </div>
+      </div>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
