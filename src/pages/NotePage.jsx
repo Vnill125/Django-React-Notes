c93a85ffab3fd3875d0d8636e7580a565ca79b20
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-14 22:20:39 +0100

    Update NotePage.jsx

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -14,17 +14,47 @@ const NotePage = () => {
     }, [])
 
     let getNote = async () => {
-        let response = await fetch(`http://127.0.0.1:8000/api/notes/${noteId}`)
+        if (noteId === 'new') return
+        
+        let response = await fetch(`http://127.0.0.1:8000/api/notes/${noteId}/`)
         let data = await response.json()
         setNote(data)
+    }
+
+    let createNote = async () => {
+      fetch('http://127.0.0.1:8000/api/notes/create/',{
+        method: 'POST',
+        headers: {
+          'Content-type':'application/json'
+        },
+        body: JSON.stringify(note)
+      })
+  } 
+
+    let updateNote = async () =>{
+      fetch(`http://127.0.0.1:8000/api/notes/${noteId}/update/`, {
+        method: 'PUT',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(note)
+      })
+    }
+
+    let deleteNote = async () => {
+      fetch(`http://127.0.0.1:8000/api/notes/${noteId}/delete/`, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+      })
     }
 
   return (
     <div className='note'>
       <div className='note-header'>
         <h3>
-          <Link to={'/'}><FaArrowLeft/></Link>
+          <Link to={'/'}><FaArrowLeft onClick={updateNote}/></Link>
         </h3>
+        {noteId !== 'new' ? 
+          <Link to={'/'}><button onClick={deleteNote}>Delete</button></Link>
+        : <Link to={'/'}><button onClick={createNote}>Add Note</button></Link>}
       </div>
       <textarea onChange={(e) => {setNote({...note, 'body':e.target.value}) }} defaultValue={note?.body}></textarea>
     </div>
