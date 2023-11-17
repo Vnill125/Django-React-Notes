f60e4fe034e57cfbd678e520e3c6ac29780c8b4e
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-17 18:55:52 +0100

    Update models.py

--- mynotes/api/models.py
+++ mynotes/api/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Note(models.Model):
+class Notes(models.Model):
+    id = models.AutoField(primary_key=True)
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
