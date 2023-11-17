b2f2eeeaf02850cea0cd5a3d9747e56a62eeb034
Krystian Muszynski <129762972+Vnill125@users.noreply.github.com>
2023-11-17 18:56:52 +0100

    Update urls.py

--- mynotes/mynotes/urls.py
+++ mynotes/mynotes/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mynotes project.
+"""mynotes URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path("admin/", admin.site.urls),
+    path("api/", include("api.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
